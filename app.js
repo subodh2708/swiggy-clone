fd73d644132ea92a341b6ee2bf3e97171283d912
Subodh <subodhlst@gmail.com>
2025-08-23 08:39:09 +0530

    create nested structure

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
-console.log(heading);
+const parent = React.createElement("div", { id: "heading" }, [
+  React.createElement("div", { id: "child-1" }, [
+    React.createElement("h1", {}, "I am a H1 element"),
+    React.createElement("h2", {}, "I am a H2 element"),
+  ]),
+  React.createElement("div", { id: "child-2" }, [
+    React.createElement("h1", {}, "I am a H1 element"),
+    React.createElement("h2", {}, "I am a H2 element"),
+  ]),
+]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
