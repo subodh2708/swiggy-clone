d28db0fe1e3059a2af8b4da177d46db1809a987b
Subodh <subodhlst@gmail.com>
2025-08-25 21:57:33 +0530

    introduce JSX and write first attribute in jsx

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 
 // Create a react element => it is an object => reactDOM renders this object as HTML element
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React🎉");
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React🎉");
+
+const jsxHeading = <h1 className="head">Namaste React with JSX</h1>;
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
