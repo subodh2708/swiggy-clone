0301b0d5ee960581c255462fa5c7b5c5d8e55fbb
Subodh <subodhlst@gmail.com>
2025-09-01 07:38:04 +0530

    add shimmer component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,34 @@
 import ResturantCard from "./ResturantCard";
-import restList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resturantsList, setResturantsList] = useState(restList);
+  const [resturantsList, setResturantsList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async function () {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7855384&lng=80.969182&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+
+    const restaurantCard = json?.data?.cards?.find(
+      (el) => el?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setResturantsList(
+      restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
+    );
+  };
+
+  if (!resturantsList.length) {
+    return <Shimmer />;
+  }
+
   return (
     <div>
       <button
