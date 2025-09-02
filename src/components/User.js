ce288ad80e555cc5b6642bc7a516354cfe102bc7
Subodh <subodhlst@gmail.com>
2025-09-02 11:18:08 +0530

    create state in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function User(props) {
   const { name, location } = props;
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div>
+      <h2>Count : {count}</h2>
+      <h2>Count : {count2}</h2>
       <h1>{name}</h1>
       <h3>{location}</h3>
       <h3>instagram: subodh0611</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,12 +2,21 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count2 } = this.state;
+
     return (
       <div>
         <h1>{name}</h1>
+        <h2>{this.state.count}</h2>
+        <h2>{count2}</h2>
         <h3>{location}</h3>
         <h3>instagram: subodh0611</h3>
       </div>
