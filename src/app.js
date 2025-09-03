08d238a3c238ad0e4047da44dae04c7e563fcc88
Subodh <subodhlst@gmail.com>
2025-09-03 11:51:35 +0530

    lazy loading with lazy function and Suspense component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+// ✅ Lazy import OUTSIDE component
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   return (
@@ -23,7 +25,14 @@ const appRouter = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Body /> },
-      { path: "/about", element: <About /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
       { path: "/contact", element: <Contact /> },
       { path: "/restaurant/:resid", element: <RestaurantMenu /> },
     ],
@@ -32,5 +41,4 @@ const appRouter = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<RouterProvider router={appRouter} />);
