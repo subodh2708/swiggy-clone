b87a5a56dc11ff6b99a47c76a070ac77fb0bb0bc
Subodh <subodhlst@gmail.com>
2025-09-07 05:04:41 +0530

    add another run test for input name

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -3,16 +3,22 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("contact us test case", () => {
-  test("contact should be rendered", () => {
+  it("should load heading in contact component", () => {
     render(<Contact />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
   });
 
-  test("should load button inside component", () => {
+  it("should load button inside contact component", () => {
     render(<Contact />);
 
     const submit = screen.getByText("Submit");
     expect(submit).toBeInTheDocument();
+  });
+
+  it("should load input name inside contact component", () => {
+    render(<Contact />);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
   });
 });
