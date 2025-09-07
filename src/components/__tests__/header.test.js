d6b2eebf54d1359066cebda6e3e3c405fda5c2be
Subodh <subodhlst@gmail.com>
2025-09-07 14:12:13 +0530

    run test for header login button

---
+++ src/components/__tests__/header.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("should render header component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "login" });
+
+  //const loginButton = screen.getByText("Login")
+
+  expect(loginButton).toBeInTheDocument();
+});
