15c4326033255d8c8a63ae8acc174009db745bc3
Subodh <subodhlst@gmail.com>
2025-09-07 14:24:51 +0530

    run test for cart items and logout button

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -19,4 +19,40 @@ it("should render header component with login button", () => {
   //const loginButton = screen.getByText("Login")
 
   expect(loginButton).toBeInTheDocument();
+});
+
+it("should render header component with cart item", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItem = screen.getByText("Cart - (0 items)");
+
+  //const loginButton = screen.getByText("Login")
+
+  expect(cartItem).toBeInTheDocument();
+});
+
+it("should render header component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "login" });
+
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "logout" });
+
+  //const loginButton = screen.getByText("Login")
+
+  expect(logoutButton).toBeInTheDocument();
 });
